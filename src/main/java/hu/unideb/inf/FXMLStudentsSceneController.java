25389a63ee96f2705093e88bb01ac764512d4ac0
Szabó Gedeon <gedeonszabo07@gmail.com>
2022-03-18 11:45:05 +0100

    1.1.1-SNAPSHOT - Hood, Trunk, Door click

--- src/main/java/hu/unideb/inf/FXMLStudentsSceneController.java
+++ src/main/java/hu/unideb/inf/FXMLStudentsSceneController.java
@@ -19,8 +19,27 @@ public class FXMLStudentsSceneController {
     @FXML
     private Label statusLabel;
 
+    // Car parts:
+
     @FXML
     private Rectangle hood;
+
+    @FXML
+    private Rectangle frDoor; // front
+
+    @FXML
+    private Rectangle flDoor;
+
+    @FXML
+    private Rectangle brDoor; // back
+
+    @FXML
+    private Rectangle blDoor;
+
+    @FXML
+    private Rectangle trunk;
+
+    // End of Car parts
 
     @FXML
     private TextField crashUserName;
@@ -64,9 +83,65 @@ public class FXMLStudentsSceneController {
     void handleHoodClicked() {
         if (hood.getStyle().equals("-fx-fill: #ffc2c2;")) {
             hood.setStyle("-fx-fill: white;");
+            System.out.println("Declicked!");
         } else {
             hood.setStyle("-fx-fill: #ffc2c2;");
-            // státusz mentése adatbázisra
+            System.out.println("Hood Clicked!");
+        }
+    }
+
+    @FXML
+    void handleTrunkClicked() {
+        if (trunk.getStyle().equals("-fx-fill: #ffc2c2;")) {
+            trunk.setStyle("-fx-fill: white;");
+            System.out.println("Declicked!");
+        } else {
+            trunk.setStyle("-fx-fill: #ffc2c2;");
+            System.out.println("Trunk Clicked!");
+        }
+    }
+
+    @FXML
+    void handleFrontRightDoor() {
+        if (frDoor.getStyle().equals("-fx-fill: #ffc2c2;")) {
+            frDoor.setStyle("-fx-fill: white;");
+            System.out.println("Declicked!");
+        } else {
+            frDoor.setStyle("-fx-fill: #ffc2c2;");
+            System.out.println("frDoor Clicked!");
+        }
+    }
+
+    @FXML
+    void handleFrontLeftDoor() {
+        if (flDoor.getStyle().equals("-fx-fill: #ffc2c2;")) {
+            flDoor.setStyle("-fx-fill: white;");
+            System.out.println("Declicked!");
+        } else {
+            flDoor.setStyle("-fx-fill: #ffc2c2;");
+            System.out.println("flDoor Clicked!");
+        }
+    }
+
+    @FXML
+    void handleBackRightDoor() {
+        if (brDoor.getStyle().equals("-fx-fill: #ffc2c2;")) {
+            brDoor.setStyle("-fx-fill: white;");
+            System.out.println("Declicked!");
+        } else {
+            brDoor.setStyle("-fx-fill: #ffc2c2;");
+            System.out.println("brDoor Clicked!");
+        }
+    }
+
+    @FXML
+    void handleBackLeftDoor() {
+        if (blDoor.getStyle().equals("-fx-fill: #ffc2c2;")) {
+            blDoor.setStyle("-fx-fill: white;");
+            System.out.println("Declicked!");
+        } else {
+            blDoor.setStyle("-fx-fill: #ffc2c2;");
+            System.out.println("blDoor Clicked!");
         }
     }
 
