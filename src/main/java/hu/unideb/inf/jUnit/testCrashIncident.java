e4d4b1f69069d00c23667d4e7a451c16142ee1f9
Dorsonszky Mate <dormateka@gmail.com>
2022-03-19 22:37:38 +0200

    Optimalizalas

--- src/main/java/hu/unideb/inf/jUnit/testCrashIncident.java
+++ src/main/java/hu/unideb/inf/jUnit/testCrashIncident.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 public class testCrashIncident {
     public static void main(String[] args) {
         test_crashIncident();
+        //test_incident();
     }
     //tovabbi setterek es getterek tesztelese
 
