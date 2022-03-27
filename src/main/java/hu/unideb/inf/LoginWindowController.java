668948e8274eb8090132608b3eaf6b95ffd387de
CzNorbi <norbertczier@gmail.com>
2022-03-27 13:58:59 +0200

    Basic Login Window

---
+++ src/main/java/hu/unideb/inf/LoginWindowController.java
@@ -0,0 +1,20 @@
+package hu.unideb.inf;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private RadioButton Admin;
+
+    @FXML
+    private TextField Name;
+
+    @FXML
+    private TextField Password;
+
+    @FXML
+    private RadioButton User;
+}
