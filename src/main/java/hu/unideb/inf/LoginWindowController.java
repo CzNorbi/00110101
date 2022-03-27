86e4006127c731268ee36c3b057ab44be5b336b5
CzNorbi <norbertczier@gmail.com>
2022-03-27 15:06:44 +0200

    Load MainWindow after login

--- src/main/java/hu/unideb/inf/LoginWindowController.java
+++ src/main/java/hu/unideb/inf/LoginWindowController.java
@@ -1,13 +1,27 @@
 package hu.unideb.inf;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginWindowController {
 
     @FXML
-    private RadioButton Admin;
+    private AnchorPane mainAnchorPane;
+
+    @FXML
+    private RadioButton AdminMode;
+
+    @FXML
+    private RadioButton UserMode;
 
     @FXML
     private TextField Name;
@@ -16,5 +30,26 @@ public class LoginWindowController {
     private TextField Password;
 
     @FXML
-    private RadioButton User;
+    private Button LoginButton;
+
+    boolean checkInput(TextField Name, TextField Password) {
+        return true;
+    }
+
+    @FXML
+    void loadMainWindow() throws IOException{
+        Stage stage =  (Stage) LoginButton.getScene().getWindow();;
+        Parent root;
+
+        root = FXMLLoader.load(getClass().getResource("/view/MainWindow.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void handleLoginButton() throws IOException {
+        loadMainWindow();
+    }
 }
