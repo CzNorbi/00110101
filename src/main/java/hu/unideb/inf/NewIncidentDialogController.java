2e7ba8f2caccd9c4aa31e0f07cca2f837afbae27
CzNorbi <norbertczier@gmail.com>
2022-04-26 19:40:30 +0200

    NewIncidentDialogController

--- src/main/java/hu/unideb/inf/NewIncidentDialogController.java
+++ src/main/java/hu/unideb/inf/NewIncidentDialogController.java
@@ -1,4 +1,263 @@
 package hu.unideb.inf;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
 public class NewIncidentDialogController {
+
+    @FXML
+    private VBox A_car;
+
+    @FXML
+    private VBox B_car;
+
+    @FXML
+    private TextArea aAddress;
+
+    @FXML
+    private Rectangle aBackLeftDoor;
+
+    @FXML
+    private Rectangle aBackLeftWheel;
+
+    @FXML
+    private Rectangle aBackRightDoor;
+
+    @FXML
+    private Rectangle aBackRightWheel;
+
+    @FXML
+    private Rectangle aBackWindshield;
+
+    @FXML
+    private TextArea aComment;
+
+    @FXML
+    private Rectangle aFrontLeftDoor;
+
+    @FXML
+    private Rectangle aFrontLeftWheel;
+
+    @FXML
+    private Rectangle aFrontRightDoor;
+
+    @FXML
+    private Rectangle aFrontRightWheel;
+
+    @FXML
+    private Rectangle aFrontWindshield;
+
+    @FXML
+    private Rectangle aHood;
+
+    @FXML
+    private TextArea aInsurer;
+
+    @FXML
+    private TextField aName;
+
+    @FXML
+    private TextField aPhone;
+
+    @FXML
+    private Rectangle aRoof;
+
+    @FXML
+    private Rectangle aTrunk;
+
+    @FXML
+    private TextArea bAddress;
+
+    @FXML
+    private Rectangle bBackLeftDoor;
+
+    @FXML
+    private Rectangle bBackLeftWheel;
+
+    @FXML
+    private Rectangle bBackRightDoor;
+
+    @FXML
+    private Rectangle bBackRightWheel;
+
+    @FXML
+    private Rectangle bBackWindshield;
+
+    @FXML
+    private TextArea bComment;
+
+    @FXML
+    private Rectangle bFrontLeftDoor;
+
+    @FXML
+    private Rectangle bFrontLeftWheel;
+
+    @FXML
+    private Rectangle bFrontRightDoor;
+
+    @FXML
+    private Rectangle bFrontRightWheel;
+
+    @FXML
+    private Rectangle bFrontWindshield;
+
+    @FXML
+    private Rectangle bHood;
+
+    @FXML
+    private TextArea bInsurer;
+
+    @FXML
+    private TextField bName;
+
+    @FXML
+    private TextField bPhone;
+
+    @FXML
+    private Rectangle bRoof;
+
+    @FXML
+    private Rectangle bTrunk;
+
+    @FXML
+    private Label locationOfIncident;
+
+    @FXML
+    private Label timeOfIncident;
+
+    @FXML
+    void handleABackLeftDoorClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleABackLeftWheelClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleABackRightDoorClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleABackRightWheelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleABackWindshieldClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleAFrontLeftDoorClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleAFrontLeftWheelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleAFrontRightDoorClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleAFrontRightWheelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleAFrontWindshieldClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleAHoodClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleARoofClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleATrunkClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBBackLeftDoorClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBBackLeftWheelClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBBackRightDoorClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBBackRightWheelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBBackWindshieldClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBFrontLeftDoorClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBFrontLeftWheelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBFrontRightDoorClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBFrontRightWheelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBFrontWindshieldClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBHoodClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBRoofClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleBTrunkClicked(MouseEvent event) {
+
+    }
+
 }
