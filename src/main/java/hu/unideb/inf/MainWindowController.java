479099fde39c5f405cad33ee9256c069624b8778
Gedeon <gedeonszabo07@gmail.com>
2022-05-06 23:24:35 +0200

    Icon & Logo added

--- src/main/java/hu/unideb/inf/MainWindowController.java
+++ src/main/java/hu/unideb/inf/MainWindowController.java
@@ -11,7 +11,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
